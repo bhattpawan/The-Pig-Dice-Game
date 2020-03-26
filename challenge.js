4cb4e62d3f39b45f63f29ece284bf66071e8989a
Pawan Bhatt <49295474+bhattpawan@users.noreply.github.com>
2020-03-26 20:01:09 +0530

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -4,7 +4,8 @@ var prevRoll,scores,roundScore,activePlayer,gamePlaying,lastDice,finalScore;
 finalScore = document.querySelector('.final-score').value;
 
 function initialise()
-{
+{   
+    location.reload();
     scores = [0,0];
     roundScore = 0;
     activePlayer = 0;  
